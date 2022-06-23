c37ad0ba20536af0e30caca997f7bdeee0a421ca
brunnermanu <brunner.manu@hotmail.com>
2022-06-23 08:53:07 +0200

    add databasemodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
@@ -24,6 +25,7 @@ import { AngularFireModule } from '@angular/fire/compat';
     AngularFireModule.initializeApp(environment.firebaseConfig, 'little-monstas'),
     AngularFirestoreModule, // Only required for database features
     AngularFireAuthModule, // Only required for auth features,
+    AngularFireDatabaseModule,
     AngularFireStorageModule // Only required for storage features
   ],
   providers: [],
