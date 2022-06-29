8527c40da2c262e74e7ad64d6a4776b8e835f6d0
brunnermanu <brunner.manu@hotmail.com>
2022-06-29 07:19:15 +0200

    add fontawesome and put .env into gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,6 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header/header/header.component';
 
-import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFireModule } from '@angular/fire/compat';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,12 +14,7 @@ import { AngularFireModule } from '@angular/fire/compat';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig, 'little-monstas'),
-    AngularFirestoreModule, // Only required for database features
-    AngularFireAuthModule, // Only required for auth features,
-    AngularFireDatabaseModule,
-    AngularFireStorageModule // Only required for storage features
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
